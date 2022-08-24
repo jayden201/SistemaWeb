411d78d582bb3917fbf938acef5fa7441a167537
Diego <diegoven@unicauca.edu.co>
2022-08-24 17:30:08 -0500

    Se modifico los Atributos de la clase empleado

--- src/main/java/com/example/mvcapp/informacionEmpresa/Empleado.java
+++ src/main/java/com/example/mvcapp/informacionEmpresa/Empleado.java
@@ -1,4 +1,52 @@
 package com.example.mvcapp.informacionEmpresa;
 
 public class Empleado {
+    private String nombre;
+    private int idEmpleado;
+    private String correoElectronico;
+    private String empresaEnQueTrabaja;
+    private String rolDelEmpleado;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getEmpresaEnQueTrabaja() {
+        return empresaEnQueTrabaja;
+    }
+
+    public void setEmpresaEnQueTrabaja(String empresaEnQueTrabaja) {
+        this.empresaEnQueTrabaja = empresaEnQueTrabaja;
+    }
+
+    public String getRolDelEmpleado() {
+        return rolDelEmpleado;
+    }
+
+    public void setRolDelEmpleado(String rolDelEmpleado) {
+        this.rolDelEmpleado = rolDelEmpleado;
+    }
+
+
 }
