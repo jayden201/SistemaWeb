7261c8adf2b7e20feea85e742dab919c03ebc4c4
Felipe Ladino <felagon_122@hotmail.com>
2022-08-25 17:20:55 -0500

    Agregando atributos y metodos getters y Setters a clase empresa

--- src/main/java/com/example/mvcapp/informacionEmpresa/Empresa.java
+++ src/main/java/com/example/mvcapp/informacionEmpresa/Empresa.java
@@ -1,4 +1,41 @@
 package com.example.mvcapp.informacionEmpresa;
 
 public class Empresa {
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private int telefonoEmpresa;
+    private int nitEmpresa;
+
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public int getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(int telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public int getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(int nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
 }
